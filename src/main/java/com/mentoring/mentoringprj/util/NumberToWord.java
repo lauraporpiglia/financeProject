6dfd9bfddca11b38f6a6971a394933bd868569f2
lauraPrp <laura.porpiglia@gmail.com>
2023-10-30 17:15:36 +0000

    test pass with ints, doubles to fix

--- src/main/java/com/mentoring/mentoringprj/util/NumberToWord.java
+++ src/main/java/com/mentoring/mentoringprj/util/NumberToWord.java
@@ -2,45 +2,43 @@ package com.mentoring.mentoringprj.util;
 
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class NumberToWord {
 
+    public String convertNumberToWords(double number) {
+        int intPart = (int) number;
+        int decimalPart = (int) ((number - intPart) * 100); // Assuming 2 decimal places
+        String result = "";
 
-        public String convertNumberToWords(double number) {
-            int intPart = (int) number;
-            int decimalPart = (int) ((number - intPart) * 100); // Assuming 2 decimal places
-            String result="";
+        String intPartWords = convertIntegerToWords(intPart);
+        String decimalPartWords = convertIntegerToWords(decimalPart);
 
-            String intPartWords = convertIntegerToWords(intPart);
-            String decimalPartWords = convertIntegerToWords(decimalPart);
-
-            if (intPartWords.isEmpty()) {
-                 result=decimalPartWords;
+        if (intPartWords.isEmpty()) {
+            result = decimalPartWords;
+        } else {
+            if (decimalPartWords.isEmpty()) {
+                result = intPartWords;
             } else {
-                if (decimalPartWords.isEmpty()) {
-                    result= intPartWords;
-                } else {
-                    result= intPartWords + decimalPartWords ;
-                }
+                result = intPartWords + decimalPartWords;
             }
-            if(result.endsWith("zero"))
-                result=result.substring(0,result.length()-4);
-
-            return result;
         }
+        if (result.endsWith("zero"))
+            result = result.substring(0, result.length() - 4);
+
+        return result;
+    }
 
 
     public String convertIntegerToWords(int number) {
-        if (number == 0) {
-            return "zero";
-        }
 
-        String[] units = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String[] units = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         String[] teens = {"", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
         String[] tens = {"", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 
         String words = "";
+        if (number == 0) {
+            return "zero";
+        }
         if (number >= 1000) {
             words += convertIntegerToWords(number / 1000) + " thousand ";
             number %= 1000;
@@ -55,7 +53,7 @@ public class NumberToWord {
         }
         if (number >= 11 && number <= 19) {
             words += teens[number - 10] + " ";
-            number = 0; // Since we've already accounted for the teen number
+            number = 0; // teen number already accounted
         }
         if (number > 0) {
             words += units[number] + " ";
@@ -63,5 +61,4 @@ public class NumberToWord {
 
         return words.trim();
     }
-    }
-
+}
--- src/test/java/com/mentoring/mentoringprj/util/NumberToWordsTest.java
+++ src/test/java/com/mentoring/mentoringprj/util/NumberToWordsTest.java
@@ -10,53 +10,53 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class NumberToWordsTest {
 
-    private NumberToWord digitConverter = null ;
+    private NumberToWord digitConverter = null;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         digitConverter = new NumberToWord();
     }
 
     @Test
-    public void testConvertIntegerToWords_Zero() {
-
+    void testConvertIntegerToWords_Zero() {
         assertEquals("zero", digitConverter.convertIntegerToWords(0));
+        assertEquals("zero", digitConverter.convertIntegerToWords(00));
     }
 
     @Test
-    public void testConvertIntegerToWords_SingleDigits() {
+    void testConvertIntegerToWords_SingleDigits() {
         assertEquals("one", digitConverter.convertIntegerToWords(1));
         assertEquals("nine", digitConverter.convertIntegerToWords(9));
     }
 
     @Test
-    public void testConvertIntegerToWords_Teens() {
+    void testConvertIntegerToWords_Teens() {
         assertEquals("eleven", digitConverter.convertIntegerToWords(11));
         assertEquals("nineteen", digitConverter.convertIntegerToWords(19));
     }
 
     @Test
-    public void testConvertIntegerToWords_Tens() {
+    void testConvertIntegerToWords_Tens() {
         assertEquals("twenty", digitConverter.convertIntegerToWords(20));
         assertEquals("ninety", digitConverter.convertIntegerToWords(90));
     }
 
     @Test
-    public void testConvertIntegerToWords_Hundreds() {
+    void testConvertIntegerToWords_Hundreds() {
         assertEquals("one hundred", digitConverter.convertIntegerToWords(100));
         assertEquals("five hundred", digitConverter.convertIntegerToWords(500));
         assertEquals("nine hundred", digitConverter.convertIntegerToWords(900));
     }
 
     @Test
-    public void testConvertIntegerToWords_Thousands() {
+    void testConvertIntegerToWords_Thousands() {
         assertEquals("one thousand", digitConverter.convertIntegerToWords(1000));
         assertEquals("five thousand", digitConverter.convertIntegerToWords(5000));
         assertEquals("nine thousand", digitConverter.convertIntegerToWords(9000));
     }
 
     @Test
-    public void testConvertIntegerToWords_ComplexNumbers() {
+    void testConvertIntegerToWords_ComplexNumbers() {
         assertEquals("two hundred thirty four", digitConverter.convertIntegerToWords(234));
         assertEquals("seven thousand eight hundred ninety", digitConverter.convertIntegerToWords(7890));
         assertEquals("twelve thousand three hundred forty five", digitConverter.convertIntegerToWords(12345));
@@ -75,15 +75,16 @@ class NumberToWordsTest {
             "8, eight",
             "9, nine"
     })
-    public void testConvertDigitToWord(int input, String expectedOutput) {
+    void testConvertDigitToWord(int input, String expectedOutput) {
         assertEquals(expectedOutput, digitConverter.convertNumberToWords(input));
     }
 
+    /*@todo fix, it doesnt work */
     @Disabled
-    public void testConvertNumberToWords() {
+     void testConvertNumberToWords() {
         // Example test cases for converting decimal numbers to words
         assertEquals("zero", digitConverter.convertNumberToWords(0.0));
-        assertEquals("one point two", digitConverter.convertNumberToWords(1.2));
+        assertEquals("one point(?) two", digitConverter.convertNumberToWords(1.2));
         assertEquals("two point three four", digitConverter.convertNumberToWords(2.34));
         assertEquals("three point four five", digitConverter.convertNumberToWords(3.45));
         assertEquals("four point six seven", digitConverter.convertNumberToWords(4.67));
