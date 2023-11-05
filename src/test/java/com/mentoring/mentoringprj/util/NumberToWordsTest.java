bfa15fccaa644b892ddf246b189e3718cb5f6d63
lauraPrp <laura.porpiglia@gmail.com>
2023-11-05 14:47:01 +0000

    introduced currency label in test

--- src/test/java/com/mentoring/mentoringprj/util/NumberToWordsTest.java
+++ src/test/java/com/mentoring/mentoringprj/util/NumberToWordsTest.java
@@ -11,6 +11,9 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class NumberToWordsTest {
 
+  private final static String  LABEL_POUND ="pounds";
+  private final static String  LABEL_P ="p";
+
     private NumberToWord digitConverter = null;
 
     @BeforeEach
@@ -20,52 +23,52 @@ class NumberToWordsTest {
 
     @Test
     void testconverToWords_Zero() throws Exception {
-        assertEquals("zero pounds zero p", digitConverter.converToWords(0));
+        assertEquals("zero " + LABEL_POUND + " zero "+LABEL_P, digitConverter.converToWords(0));
     }
 
     @Test
     void testconverToWords_SingleDigits() throws Exception {
-        assertEquals("one pounds zero p", digitConverter.converToWords(1));
-        assertEquals("nine pounds zero p", digitConverter.converToWords(9));
+        assertEquals("one " + LABEL_POUND + " zero "+LABEL_P, digitConverter.converToWords(1));
+        assertEquals("nine " + LABEL_POUND + " zero "+LABEL_P, digitConverter.converToWords(9));
     }
 
     @Test
     void testconverToWords_Teens() throws Exception {
-        assertEquals("eleven pounds zero p", digitConverter.converToWords(11));
-        assertEquals("nineteen pounds zero p", digitConverter.converToWords(19));
+        assertEquals("eleven " + LABEL_POUND + " zero "+LABEL_P, digitConverter.converToWords(11));
+        assertEquals("nineteen " + LABEL_POUND + " zero "+LABEL_P, digitConverter.converToWords(19));
     }
 
     @Test
     void testConversionForNumbersWithNonZeroDecimal() throws Exception {
-        assertEquals("fifty five pounds zero p", digitConverter.convertDoubleToWords("55", "pound"));
-        assertEquals("seventy two pounds zero p", digitConverter.converToWords(72));
+        assertEquals("fifty five " + LABEL_POUND + " zero "+LABEL_P, digitConverter.convertDoubleToWords("55", "pound"));
+        assertEquals("seventy two " + LABEL_POUND + " zero "+LABEL_P, digitConverter.converToWords(72));
     }
 
     @Test
     void testconverToWords_Tens() throws Exception {
-        assertEquals("twenty pounds zero p", digitConverter.converToWords(20));
-        assertEquals("ninety pounds zero p", digitConverter.converToWords(90));
+        assertEquals("twenty " + LABEL_POUND + " zero "+LABEL_P, digitConverter.converToWords(20));
+        assertEquals("ninety " + LABEL_POUND + " zero "+LABEL_P, digitConverter.converToWords(90));
     }
 
     @Test
     void testconverToWords_Hundreds() throws Exception {
-        assertEquals("one hundred pounds zero p", digitConverter.converToWords(100));
-        assertEquals("five hundred pounds zero p", digitConverter.converToWords(500));
-        assertEquals("nine hundred pounds zero p", digitConverter.converToWords(900));
+        assertEquals("one hundred " + LABEL_POUND + " zero "+LABEL_P, digitConverter.converToWords(100));
+        assertEquals("five hundred " + LABEL_POUND + " zero "+LABEL_P, digitConverter.converToWords(500));
+        assertEquals("nine hundred " + LABEL_POUND + " zero "+LABEL_P, digitConverter.converToWords(900));
     }
 
     @Test
     void testconverToWords_Thousands() throws Exception {
-        assertEquals("one thousand pounds zero p", digitConverter.converToWords(1000));
-        assertEquals("five thousand pounds zero p", digitConverter.converToWords(5000));
-        assertEquals("nine thousand pounds zero p", digitConverter.converToWords(9000));
+        assertEquals("one thousand " + LABEL_POUND + " zero "+LABEL_P, digitConverter.converToWords(1000));
+        assertEquals("five thousand " + LABEL_POUND + " zero "+LABEL_P, digitConverter.converToWords(5000));
+        assertEquals("nine thousand " + LABEL_POUND + " zero "+LABEL_P, digitConverter.converToWords(9000));
     }
 
     @Test
     void testconverToWords_ComplexNumbers() throws Exception {
-        assertEquals("two hundred thirty four pounds zero p", digitConverter.converToWords(234));
-        assertEquals("seven thousand eight hundred ninety pounds zero p", digitConverter.converToWords(7890));
-        assertEquals("twelve thousand three hundred forty five pounds zero p", digitConverter.converToWords(12345));
+        assertEquals("two hundred thirty four " + LABEL_POUND + " zero "+LABEL_P, digitConverter.converToWords(234));
+        assertEquals("seven thousand eight hundred ninety " + LABEL_POUND + " zero "+LABEL_P, digitConverter.converToWords(7890));
+        assertEquals("twelve thousand three hundred forty five " + LABEL_POUND + " zero "+LABEL_P, digitConverter.converToWords(12345));
     }
 
     @Test
@@ -76,16 +79,16 @@ class NumberToWordsTest {
 
     @ParameterizedTest
     @CsvSource({
-            "0, zero pounds zero p",
-            "1, one pounds zero p",
-            "2, two pounds zero p",
-            "3, three pounds zero p",
-            "4, four pounds zero p",
-            "5, five pounds zero p",
-            "6, six pounds zero p",
-            "7, seven pounds zero p",
-            "8, eight pounds zero p",
-            "9, nine pounds zero p"
+            "0, zero " + LABEL_POUND + " zero "+LABEL_P,
+            "1, one " + LABEL_POUND + " zero "+LABEL_P,
+            "2, two " + LABEL_POUND + " zero "+LABEL_P,
+            "3, three " + LABEL_POUND + " zero "+LABEL_P,
+            "4, four " + LABEL_POUND + " zero "+LABEL_P,
+            "5, five " + LABEL_POUND + " zero "+LABEL_P,
+            "6, six " + LABEL_POUND + " zero "+LABEL_P,
+            "7, seven " + LABEL_POUND + " zero "+LABEL_P,
+            "8, eight " + LABEL_POUND + " zero "+LABEL_P,
+            "9, nine " + LABEL_POUND + " zero "+LABEL_P
     })
     void testConvertDigitToWord(int input, String expectedOutput) throws Exception {
         assertEquals(expectedOutput, digitConverter.converToWords(input));
@@ -93,14 +96,14 @@ class NumberToWordsTest {
 
     @Test
     void testConvertDoubleToWords() throws Exception {
-        assertEquals("zero pounds zero p", digitConverter.convertDoubleToWords("0.0", "pound"));
-        assertEquals("one pounds twenty p", digitConverter.convertDoubleToWords("1.20", "pound"));
-        assertEquals("ten pounds ten p", digitConverter.convertDoubleToWords("10.10", "pound"));
+        assertEquals("zero " + LABEL_POUND + " zero "+LABEL_P, digitConverter.convertDoubleToWords("0.0", "pound"));
+        assertEquals("one " + LABEL_POUND + " twenty "+LABEL_P, digitConverter.convertDoubleToWords("1.20", "pound"));
+        assertEquals("ten " + LABEL_POUND + " ten "+LABEL_P, digitConverter.convertDoubleToWords("10.10", "pound"));
 
-        assertEquals("two pounds thirty four p", digitConverter.convertDoubleToWords("2.34", "pound"));
-        assertEquals("three pounds forty five p", digitConverter.convertDoubleToWords("3.45", "pound"));
-        assertEquals("four pounds sixty seven p", digitConverter.convertDoubleToWords("4.67", "pound"));
-        assertEquals("five pounds eighty nine p", digitConverter.convertDoubleToWords("5.89", "pound"));
+        assertEquals("two " + LABEL_POUND + " thirty four "+LABEL_P, digitConverter.convertDoubleToWords("2.34", "pound"));
+        assertEquals("three " + LABEL_POUND + " forty five "+LABEL_P, digitConverter.convertDoubleToWords("3.45", "pound"));
+        assertEquals("four " + LABEL_POUND + " sixty seven "+LABEL_P, digitConverter.convertDoubleToWords("4.67", "pound"));
+        assertEquals("five " + LABEL_POUND + " eighty nine "+LABEL_P, digitConverter.convertDoubleToWords("5.89", "pound"));
     }
 
 }
